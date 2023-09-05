c8d7f2f7efaa31ff33d72ed0bbeded1024e070ad
giulianopalmisano <giulipalmisano01@gmail.com>
2023-09-05 14:42:51 -0300

    Ej2 v3
    Documentación añadida. Corrección del período del timer.

--- TP1/Ej2/main.py
+++ TP1/Ej2/main.py
@@ -3,16 +3,23 @@
 # Ejercicio 2
 # Giuliano Palmisano
 
+'''
+Este programa lee e imprime en la consola el valor del conversor A/D cada 5 segundos.
+'''
+
+### LIBRERIAS ###
 import machine
 from machine import Timer, Pin, ADC
 
+# Configuración de ADC
 adc_pin = 26 # Corresponde a la entrada A0
 adc_A0 = ADC(adc_pin)
 
+# Función de interrupción del timer
 def timer_int(timer):
     adc_A0_value = adc_A0.read_u16()
     print(adc_A0_value) 
     
-if __name__=="__main__":
-    timer_lect_A0 = Timer(mode=Timer.PERIODIC, period=1000, callback=timer_int)
+# Configuración del timer
+timer_lect_A0 = Timer(mode=Timer.PERIODIC, period=5000, callback=timer_int)
     
