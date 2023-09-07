f84edc07a1bc211efe2fc36693acaabdc9f1c3e1
giulianopalmisano <giulipalmisano01@gmail.com>
2023-09-07 13:27:41 -0300

    TP1 Ej4 Final
    Cambio a input(). Doc completa.

--- TP1/Ej4/main.py
+++ TP1/Ej4/main.py
@@ -5,110 +5,111 @@
 
 '''
 Este programa realiza la lectura y almacenamiento de datos de los 3 convertidores A/D.
-Puede configurarse para realizar la lectura y almacenamiento cada cierto tiempo (mode 0),
-o para activar y desactivar la lectura mediante comandos (mode 1).
-Mode 0. Una vez configurado el intervalo deseado, no puede cambiarse a menos que se reinicie el programa.
-Mode 1. Mientras este activa la lectura, se toman y almacenan mediciones cada 200ms.
+Para iniciar la lectura, se debe ingresar el comando 'start' por la terminal.
+Para detener la lectura, se debe ingresar el comando 'stop' por la terminal.
+Para descargar los datos, se debe ingresar el comando 'download' por la terminal.
+Mientras este activa la lectura, se toman y almacenan mediciones con un intervalo de tiempo configurable.
 '''
 
 ### LIBRERIAS ###
-import sys, uselect, machine, time
-from machine import Pin, ADC, RTC, Timer
+from machine import Timer, Pin, ADC, RTC
 
+### PARAMETROS ###
+CONV_ADC = 3.3/65535 # Factor de converision del ADC
+T_MUESTREO_MS = 5000 # Tiempo de muestreo y almacenamiento
+# Pines de los ADC
+ADC0_PIN = 26
+ADC1_PIN = 27
+ADC2_PIN = 28
+
+### SETUP ###
 # Inicialización del reloj en tiempo real (RTC)
 rtc = RTC()
 
 # Configuración de los pines de entrada analógica
-adc_A0_pin = 26 # Corresponde a la entrada A0
-adc_A0 = ADC(adc_A0_pin)
-adc_A1_pin = 27 # Corresponde a la entrada A1
-adc_A1 = ADC(adc_A1_pin)
-adc_A2_pin = 28 # Corresponde a la entrada A2
-adc_A2 = ADC(adc_A2_pin)
+adc_A0 = ADC(ADC0_PIN)
+adc_A1 = ADC(ADC1_PIN)
+adc_A2 = ADC(ADC2_PIN)
 
 # Apertura del archivo para almacenar datos
 file = open('data/data.txt','w')
-lect = False
 
+# Creación del timer (se configura una vez iniciada la lectura)
+timer_lect = Timer(-1)
+
+lect = False # Booleano para controlar la lectura
+writing = False # Booleano para controlar la escritura del archivo
+
+### FUNCIONES ###
 # Función para la lectura de los canales ADC
-def lectura_ADCs():
-    # Lectura e impresión por consola de los datos
-    adc_A0_value = adc_A0.read_u16()
-    #volt = (3.3/65535)*adc_value
-    print("Valor A0: " + str(adc_A0_value))
-    adc_A1_value = adc_A1.read_u16()
-    print("Valor A1: " + str(adc_A1_value))
-    adc_A2_value = adc_A2.read_u16()
-    print("Valor A2: " + str(adc_A2_value))
-    
-    # Obtencion y formateo de la fecha y hora
-    date = rtc.datetime()
-    date_str = str(date[2])+"/"+str(date[1])+"/"+str(date[0])+", "+str(date[4])+":"+str(date[5])+":"+str(date[6])
-    
-    # Escribir los valores en el archivo
-    file.write(date_str + " A0: " + str(adc_A0_value) + "\n")
-    file.write(date_str + " A1: " + str(adc_A1_value) + "\n")
-    file.write(date_str + " A2: " + str(adc_A2_value) + "\n")
-    
-    file.flush()
-
-# Función para la configuración del modo de lectura
-def config_mode():
-    global mode
-    conf = False # Booleano que es verdadero una vez se elige el modo
-    while not conf:
-        init = input("Desea configurar un intervalo de lectura (y/n)? ")
-        if init == "y":
-            # Modo de lectura mediante interrupciones
-            global per_seg
-            mode = 0 
-            per_seg = int(input("Ingrese el intervalo de lectura en segundos (minimo 30 seg): "))
-            while per_seg < 30:
-                per_seg = int(input("Ingrese un intervalo mayor a 30 segundos: "))
-            print("Intervalo de "+ str(per_seg) +" seg configurado.")
-            conf = True
-        elif init == "n":
-            # Modo de lectura mediante comandos
-            mode = 1 
-            print("La lectura entonces se ejecuta mediante comandos.\n")
-            print("Ingrese 'r' para comenzar la lectura y 's' para detenerla. \n")
-            conf = True
+def lectura_ADCs(timer):
+    global writing, lect, file
+    # Compruebo que esté activada la lectura y no interrumpir el proceso de escritura
+    if lect:
+        if writing:
+            print("Lectura fallida.")
         else:
-            print("Error. Ingrese 'y' si desea configurar un intervalo de lectura o 'n' en caso contrario.\n")
+            # Lectura de los datos
+            adc_A0_value = CONV_ADC*adc_A0.read_u16()
+            adc_A1_value = CONV_ADC*adc_A1.read_u16()
+            adc_A2_value = CONV_ADC*adc_A2.read_u16()
+            
+            # Obtencion y formateo de la fecha y hora
+            date = rtc.datetime()
+            date_str = str(date[2])+"/"+str(date[1])+"/"+str(date[0])+", "+str(date[4])+":"+str(date[5])+":"+str(date[6])
+            
+            # Escribir los valores en el archivo
+            writing = True
+            file.write(date_str + " A0: " + str(adc_A0_value) + "\n")
+            file.write(date_str + " A1: " + str(adc_A1_value) + "\n")
+            file.write(date_str + " A2: " + str(adc_A2_value) + "\n")
+            file.flush()
+            writing = False
 
-# Función para leer comandos desde la terminal
-def read_com():
-    return(sys.stdin.readline(1) if spoll.poll(0) else None)
+# Función para analizar el comando introducido
+def procesar_com(fcom):
+    global lect, timer_lect, T_MUESTREO_MS
+    if fcom == "start":
+        lect = True
+        timer_lect.init(period=T_MUESTREO_MS, mode=Timer.PERIODIC, callback=lectura_ADCs) # Configuración del timer
+        print("Comenzando lectura...")
+    elif fcom == "stop":
+        lect = False
+        timer_lect.deinit() # Se deshabilita el timer
+        print("Lectura pausada.")
+        print("Ingrese 'start' para reiniciarla.")
+        print("Ingrese 'download' para visualizar las mediciones.")
+    elif fcom == "download":
+        print("Descargando...")
+        # Se abre el archivo en modo lectura y se muestra linea por linea
+        with open('data/data.txt', 'r') as data_file:
+            for line in data_file:
+                print(line.strip())
+    else:
+        print("Comando desconocido.\n")
+        print("Ingrese 'start' si desea iniciar la lectura.\n")
+        print("Ingrese 'stop' si desea detener la lectura.\n")
+        print("Ingrese 'download' si desea descargar el archivo.\n")
 
-# Función de interrupción de temporizador
-def timer_int(timer):
-    global lect
-    lect = True
+# Función para configurar el intervalo de muestreo
+def config():
+    t_seg = int(input("Ingrese intervalo de muestreo (mayor a 30 seg): "))
+    # Compruebo que el intervalo sea minimo de 30 segundos
+    while t_seg < 30:
+            t_seg = int(input("Ingrese un intervalo mayor a 30 segundos: "))
+    print("Intervalo de "+ str(t_seg) +" seg configurado.")
+    return t_seg*1000
 
-# Configuración inicial y programa principal
-config_mode()
-if mode == 0:
-    per_ms = per_seg*1000
-    timer_lect_A0 = Timer(mode=Timer.PERIODIC, period=per_ms, callback=timer_int)
-    while True:
-        if lect:
-            lectura_ADCs()
-            lect = False
-elif mode == 1:
-    spoll = uselect.poll()
-    spoll.register(sys.stdin, uselect.POLLIN)
-    while True:
-        data = read_com()
-        if data != None:
-            if data == "r":
-                lect = True
-                print("Comenzando lectura...")
-            elif data == "s":
-                lect = False
-                print("Lectura detenida.\nPresione 'r' para registrar los datos.")
-            else:
-                print("Comando inexistente.\nIngrese 'r' para comenzar la lectura o 's' para detenerla.")
-        if lect:
-            lectura_ADCs()
-        time.sleep_ms(200)
+### INICIO DEL PROGRAMA ###
+# Configuración del intervalo de muestreo
+T_MUESTREO_MS = config()
+
+print("Ingrese 'start' para inciar la lectura y 'stop' para detenerla.")
+print("Ingrese 'download' para descargar 'data.txt'")
+
+### MAIN LOOP ###
+# Se esperan los comandos del usuario
+while True:
+    com = input(">:")
+    procesar_com(com)
 file.close()
